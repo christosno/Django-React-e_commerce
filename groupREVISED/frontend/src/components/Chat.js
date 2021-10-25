04a1b73ea5ffd8dfea00175f2a587b17affd303a
Christos Nounis <73112720+christosno@users.noreply.github.com>
2021-10-25 11:48:30 +0300

    Update Chat.js

--- groupREVISED/frontend/src/components/Chat.js
+++ groupREVISED/frontend/src/components/Chat.js
@@ -1,14 +1,6 @@
-
-import ChatBody from './ChatBody';
-
-import { useHistory } from 'react-router-dom';
-import { Container, Row, Col, Image, Form, Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import axios from "axios";
-import {ToggleButtonGroup} from "react-bootstrap";
-
+import { Container, Col, Button } from "react-bootstrap";
 import React , { useEffect , useState ,useRef} from "react";
-import { db , auth} from "../firebase";
+import { db } from "../firebase";
 import ChatInput from './ChatInput';
 
 function Chat(){
@@ -25,7 +17,7 @@ function Chat(){
     },[])
 
     useEffect(() => {
-        scroll.current?.scrollIntoView({ behavior: 'instant' }); //smooth gia na pigainei pio arga
+        scroll.current?.scrollIntoView({ behavior: 'instant' }); //smooth 
       });
 
     function chatDisplay() {
@@ -70,12 +62,11 @@ function Chat(){
                     <Col className="head-img" style={{fontSize:"18px",textAlign:"center"}} >
                         <h2>Welcome to ChatRoom</h2>
                     </Col> 
-                
                     <Col style={{marginBottom:"6rem",marginTop:"4rem"}}>
                         <div style={{fontSize:"15px"}}>
-                            {messages.map(({id,text})=>( //add protoURL if wanted
+                            {messages.map(({id,text})=>(
                                 <div key={id} className='msg'>
-                                    {/* <img src = {photoURL}  alt="user image"/> */}
+                                
                                     
                                     <strong>{loggedUser.name} texted:</strong>
                                      <p>{text}</p>
@@ -91,12 +82,8 @@ function Chat(){
             :
             ""
             }
-       
         </>
     )
 }
 
 export default Chat
-
-
-
